eef0045cb02769bd9d8499d0b21f19582beb2fc8
anchoragewonder <anchoragewonder@gmail.com>
2020-11-10 11:39:05 -0500

    reformat quiz

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 var quizQuestions;
 
+
 // return JSON data from any file path (asynchronous)
 async function getJSON(path) {
     let json = await fetch(path).then(response => response.json());
     createJSObject(json);
 }
 
-getJSON("https://kauhny1enj.execute-api.us-east-1.amazonaws.com/Prod").then(data => {
+getJSON("https://kauhny1enj.execute-api.us-east-1.amazonaws.com/Stage/champion").then(data => {
     champList = data;
+    console.log("Success")
 })
+
