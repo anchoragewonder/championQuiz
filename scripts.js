54ba8b388a25469b8ff7e090751337e773faadb3
anchoragewonder <anchoragewonder@gmail.com>
2020-11-10 16:15:27 -0500

    quiz cloning complete

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-
-
+var quiz_card_prefab;
+var quizData;
+var quizLength;
 
 // return JSON data from any file path (asynchronous)
 async function getJSON(path) {
@@ -9,44 +10,46 @@ async function getJSON(path) {
 
 function createJSObject(json) {
 
-    dict = new Object();
-
-    for (let i = 0; i < json.quiz.length; i++) {
-
-        var quizTemplate = json.quiz[i];
-        var quizQuestion = quizTemplate.question;
-        var responses = quizTemplate.responses;
-        var attribute = quizTemplate.quizTemplate
-
-        if (!(quizQuestion in dict)) {
-            dict[quizQuestion] = [];
-        }
-
-        if (!(responses in dict)) {
-            dict[responses] = [];
-        }
-
-        if (!(attribute in dict)) {
-            dict[attribute] = [];
-        }
-        dict[quizQuestion].push(quizTemplate);
-        dict[responses].push(quizTemplate);
-        dict[attribute].push(quizTemplate);
-    }
-    console.log(dict);
+    quizData = json.quiz;
+    quizLength = json.quiz.length;
+    console.log(quizData[0].question);
+    console.log(quizLength);
 }
 
-// need to fix cors policy to allow cross origin access I think
-/*getJSON("https://kauhny1enj.execute-api.us-east-1.amazonaws.com/Stage/champion").then(data => {
-    champList = data;
-    console.log("Success")
-})*/
-
-// get local json of quiz function
-getJSON()
+getJSON("https://raw.githubusercontent.com/anchoragewonder/championQuiz/main/quiz.json").then(data => {
+    console.log("Success");
+})
 
 $(document).ready(function () {
     quiz_card_prefab = document.getElementById('quiz_card').cloneNode(true);
 
+    var parent = document.getElementById('quiz_start');
+    parent.innerHTML = '';
 
+    $(".btn").click(function () {
+        var startButton = document.getElementById('start_btn');
+        startButton.innerHTML = '';
+
+        for (let i = 0; i < quizLength; i++) {
+            let quiz_card = quiz_card_prefab.cloneNode(true);
+
+            // defining varibles to eaily acces ditionary and populate the clone card
+            let questionHeader = quizData[i].question;
+            let questionAttr = quizData[i].attribute;
+            let questionRes = quizData[i].responses;
+
+            // need to optimize : inputs text from dictionary into button id in cloned element
+            $(quiz_card).find("#button_1").text(questionRes[0].text)
+            $(quiz_card).find("#button_2").text(questionRes[1].text)
+            $(quiz_card).find("#button_3").text(questionRes[2].text)
+            $(quiz_card).find("#button_4").text(questionRes[3].text)
+
+            // populates card clone with the question header from dictionary
+            $(quiz_card).find("#cardQuestion").text(questionHeader)
+
+            // at the end of each for loop create new cloned element with varibles from dictionary
+            document.body.appendChild(quiz_card);
+        }
+    })
 })
+
