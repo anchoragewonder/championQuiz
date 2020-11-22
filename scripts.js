f5cca5d7c7f405fe1e264d9e401574eade05a70c
anchoragewonder <anchoragewonder@gmail.com>
2020-11-22 15:35:56 -0500

    setting values into array

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 var quiz_card_prefab;
 var quizData;
 var quizLength;
+var answers = [];
+var attribute = [];
 
 // return JSON data from any file path (asynchronous)
 async function getJSON(path) {
@@ -9,7 +11,6 @@ async function getJSON(path) {
 }
 
 function createJSObject(json) {
-
     quizData = json.quiz;
     quizLength = json.quiz.length;
     console.log(quizData[0].question);
@@ -37,19 +38,36 @@ $(document).ready(function () {
             let questionHeader = quizData[i].question;
             let questionAttr = quizData[i].attribute;
             let questionRes = quizData[i].responses;
+            var secretValA = questionRes[0].value;
+            var secretValB = questionRes[1].value;
+            var secretValC = questionRes[2].value;
+            var secretValD = questionRes[3].value;
 
             // need to optimize : inputs text from dictionary into button id in cloned element
-            $(quiz_card).find("#button_1").text(questionRes[0].text)
-            $(quiz_card).find("#button_2").text(questionRes[1].text)
-            $(quiz_card).find("#button_3").text(questionRes[2].text)
-            $(quiz_card).find("#button_4").text(questionRes[3].text)
+            $(quiz_card).find("#button_1").text(questionRes[0].text).addClass(`question${i}`).attr('value', secretValA);
+            $(quiz_card).find("#button_2").text(questionRes[1].text).addClass(`question${i}`).attr('value', secretValB);
+            $(quiz_card).find("#button_3").text(questionRes[2].text).addClass(`question${i}`).attr('value', secretValC);
+            $(quiz_card).find("#button_4").text(questionRes[3].text).addClass(`question${i}`).attr('value', secretValD);
+
 
             // populates card clone with the question header from dictionary
-            $(quiz_card).find("#cardQuestion").text(questionHeader)
-
+            $(quiz_card).find("#cardQuestion").text(questionHeader);
+            $(quiz_card).find("#cardAtrr").text(questionAttr);
             // at the end of each for loop create new cloned element with varibles from dictionary
             document.body.appendChild(quiz_card);
         }
     })
 })
 
+function buttonClick(obj, btnNum) {
+    var buttonPressed = btnNum;
+    var btnClass = obj.className;
+    var num = btnClass.match(/\d/g);
+    var btnVal = obj.value;
+    num = parseInt(num.join(''));
+
+    answers[num] = btnVal;
+
+    console.log(answers)
+    console.log(num);
+}
